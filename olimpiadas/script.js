1981600a5ef7175058ff5f4a807f659088ea2c3d
Iglyson <iglyson.andrey@escolar.ifrn.edu.br>
2022-11-17 11:25:04 -0300

    first commit

---
+++ olimpiadas/script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu");
+const navmenu = document.querySelector(".navmenu");
+
+menu.addEventListener("click",() =>{
+    menu.classList.toggle('active')
+    menu.classList.toggle('active')
+})
