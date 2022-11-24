257e29231ca82d80c19d99a09a6894e57482672a
iglyson <118455831+Iglyson@users.noreply.github.com>
2022-11-23 21:51:48 -0300

    Delete olimpiadas directory

--- olimpiadas/script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector(".menu");
-const navmenu = document.querySelector(".navmenu");
-
-menu.addEventListener("click",() =>{
-    menu.classList.toggle('active')
-    menu.classList.toggle('active')
-})
